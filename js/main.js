10195531b0c65ddf03be60fde13f140751f92003
Pawel <pchyra88@gmail.com>
2019-02-09 14:44:56 +0100

    warunkiJs

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var number = 12;
+console.log(number);
+if (number>10) {
+    console.log("warunek spełniony");
+    }else{
+        console.log("warunek niespełniony");
+    }
+    console.log("Podana liczba jest większa od 10");
