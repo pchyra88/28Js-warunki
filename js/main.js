81ee28b326f65f62a2379c78a82b11591360df07
Pawel <pchyra88@gmail.com>
2019-02-11 19:55:48 +0100

    28popr

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-var number = 12;
-console.log(number);
-if (number>10) {
-    console.log("warunek spełniony");
-    }else{
-        console.log("warunek niespełniony");
-    }
+let numer = 12;
+console.log(numer);
+if (numer>10) {
     console.log("Podana liczba jest większa od 10");
+    }else{
+        console.log("Podana liczba nie jest większa od 10");
+    }
